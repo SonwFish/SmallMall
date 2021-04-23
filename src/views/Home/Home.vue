6e485b646752bbb999b934764aa5c0aa461828dc
SnowFish <1124042943@qq.com>
2021-04-23 20:13:03 +0800

    '更新商城首页'

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,43 +3,121 @@
     <NavNar class="home-nav"><div slot="center">购物街</div></NavNar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control"
+                 :titles="['流行','新款','精选']"
+                 @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+
   </div>
+
 </template>
 
 <script>
   import NavNar from "@/components/common/navbar/NavNar";
+
   import HomeSwiper from "@/views/Home/childComps/HomeSwiper";
   import RecommendView from "@/views/Home/childComps/RecommendView";
+  import FeatureView from "@/views/Home/childComps/FeatureView";
 
-  import {getHomeMultidata} from "@/network/home";
+  import TabControl from "@/components/content/tabControl/TabControl";
+  import GoodsList from "@/components/content/goods/GoodsList";
+
+  import {getHomeMultidata,getHomeGoods} from "@/network/home";
 
 
   export default {
   name: "Home",
   components: {
+    FeatureView,
     RecommendView,
     NavNar,
-    HomeSwiper
+    HomeSwiper,
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banners: [],
-      recommends:[]
+      recommends:[],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      },
+      currentType: 'pop'
+    }
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
     }
   },
   created() {
     // 1.请求多个数据
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list,
-      this.recommends = res.data.recommend.list
-    })
-  }
+    this.getHomeMultidata()
+
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+    methods: {
+      //事件监听相关的方法
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+                break
+          case 1:
+            this.currentType = 'new'
+                break
+          case 2:
+            this.currentType = 'sell'
+                break
+        }
+      },
+
+      // 网络请求相关的方法
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list,
+            this.recommends = res.data.recommend.list
+        })
+      },
+
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type,page).then(res => {
+          // console.log(res.data.list);
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1
+        })
+      }
+    }
 }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 </style>
