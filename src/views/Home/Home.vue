bb411a151152909923598a130cad0f2f63b400df
SnowFish <1124042943@qq.com>
2021-04-23 10:26:50 +0800

    '轮播图开发'

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <NavNar class="home-nav"><div slot="center">购物街</div></NavNar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "Home"
+  import NavNar from "@/components/common/navbar/NavNar";
+  import HomeSwiper from "@/views/Home/childComps/HomeSwiper";
+  import RecommendView from "@/views/Home/childComps/RecommendView";
+
+  import {getHomeMultidata} from "@/network/home";
+
+
+  export default {
+  name: "Home",
+  components: {
+    RecommendView,
+    NavNar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends:[]
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list,
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
