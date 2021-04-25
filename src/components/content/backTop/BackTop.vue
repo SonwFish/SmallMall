e055c0d16e9b794094975e3728eba3442fb5316f
SnowFish <1124042943@qq.com>
2021-04-25 10:50:29 +0800

    '首页开发更新'

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="back-top" @click="backClick">
+    <img src="~assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackTop",
+  methods: {
+    backClick() {
+      // console.log('BackClick');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .back-top {
+    position: fixed;
+    right: 10px;
+    bottom: 50px;
+  }
+  .back-top img {
+    width: 43px;
+  }
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="home">
+    <!--  导航栏组件-->
     <NavNar class="home-nav"><div slot="center">购物街</div></NavNar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control"
-                 :titles="['流行','新款','精选']"
-                 @tabClick="tabClick"
-    ></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
 
+    <!--滚动Scroll部分-->
+    <scroll class="content" ref="scroll" :probe-type="3" :pull-up-load="true"
+            @scroll="contentScroll" @pullingUp="loadMore">
+      <!--轮播图组件-->
+      <home-swiper :banners="banners"></home-swiper>
+      <!--推荐产品组件-->
+      <recommend-view :recommends="recommends"></recommend-view>
+      <!--特色产品组件-->
+      <feature-view></feature-view>
+
+      <!--导航控制组件-->
+      <tab-control class="tab-control"
+                   :titles="['流行','新款','精选']"
+                   @tabClick="tabClick">
+      </tab-control>
+      <!--商品列表组件-->
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+
+    <!--返回顶部键组件-->
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 
 </template>
@@ -23,6 +37,8 @@
 
   import TabControl from "@/components/content/tabControl/TabControl";
   import GoodsList from "@/components/content/goods/GoodsList";
+  import Scroll from "@/components/common/scroll/Scroll";
+  import BackTop from "@/components/content/backTop/BackTop";
 
   import {getHomeMultidata,getHomeGoods} from "@/network/home";
 
@@ -35,7 +51,9 @@
     NavNar,
     HomeSwiper,
     TabControl,
-    GoodsList
+    GoodsList,
+    Scroll,
+    BackTop
   },
   data() {
     return {
@@ -46,7 +64,8 @@
         'new': {page: 0, list: []},
         'sell': {page: 0, list: []}
       },
-      currentType: 'pop'
+      currentType: 'pop',
+      isShowBackTop: false
     }
   },
   computed: {
@@ -79,6 +98,21 @@
         }
       },
 
+      // 回到顶部
+      backClick() {
+        this.$refs.scroll.scrollTo(0,0,500)
+      },
+
+      // 判断返回图标是否显示
+      contentScroll(positon) {
+        this.isShowBackTop = -positon.y > 1000
+      },
+
+      //上拉加载更多商品
+      loadMore() {
+        this.getHomeGoods(this.currentType)
+      },
+
       // 网络请求相关的方法
       getHomeMultidata() {
         getHomeMultidata().then(res => {
@@ -88,20 +122,25 @@
       },
 
       getHomeGoods(type) {
+        //获取页数，并且页数加一
         const page = this.goods[type].page + 1
+
         getHomeGoods(type,page).then(res => {
-          // console.log(res.data.list);
           this.goods[type].list.push(...res.data.list)
           this.goods[type].page += 1
+
+          this.$refs.scroll.scroll.refresh()
+          this.$refs.scroll.finishPullUp()
         })
       }
-    }
+    },
 }
 </script>
 
 <style scoped>
   #home {
-    padding-top: 44px;
+    /*padding-top: 44px;*/
+    height: 100vh;
   }
 
   .home-nav {
@@ -116,8 +155,18 @@
   }
 
   .tab-control {
-    position: sticky;
+    /*position: sticky;*/
     top: 44px;
     z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
 </style>
